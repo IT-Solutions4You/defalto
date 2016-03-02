5dec5c6aa5bb2ce7b2d93610636c50fd787b21bc
Preexo <tim.niklas@web.de>
2016-03-02 16:36:49 +0800

    takes validation of senseless values from user preferences into admin user edit

--- layouts/vlayout/modules/Users/resources/Edit.js
+++ layouts/vlayout/modules/Users/resources/Edit.js
@@ -67,6 +67,12 @@ Vtiger_Edit_Js("Users_Edit_Js",{},{
 	registerRecordPreSaveEvent : function(form){
 		var thisInstance = this;
 		form.on(Vtiger_Edit_Js.recordPreSave, function(e, data) {
+			var groupingSeperatorValue = jQuery('[name="currency_grouping_separator"]', form).val();
+			var decimalSeperatorValue = jQuery('[name="currency_decimal_separator"]', form).val();
+			if(groupingSeperatorValue == decimalSeperatorValue){
+				Vtiger_Helper_Js.showPnotify(app.vtranslate('JS_DECIMAL_SEPERATOR_AND_GROUPING_SEPERATOR_CANT_BE_SAME'));
+				e.preventDefault();
+			}
 			var userName = jQuery('input[name="user_name"]').val();
 			var newPassword = jQuery('input[name="user_password"]').val();
 			var confirmPassword = jQuery('input[name="confirm_password"]').val();
