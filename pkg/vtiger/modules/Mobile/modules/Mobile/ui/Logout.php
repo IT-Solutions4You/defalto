6d588f07252d02baf0616aa2792db63df1db6fa6
Preexo <tim.niklas@web.de>
2016-05-13 17:56:10 +0800

    added all missing Session -> Session2 updates

--- pkg/vtiger/modules/Mobile/modules/Mobile/ui/Logout.php
+++ pkg/vtiger/modules/Mobile/modules/Mobile/ui/Logout.php
@@ -11,7 +11,7 @@
 class Mobile_UI_Logout extends Mobile_WS_Controller {
 	
 	function process(Mobile_API_Request $request) {
-		HTTP_Session::destroy(HTTP_Session::detectId());
+		HTTP_Session2::destroy(HTTP_Session2::detectId());
 		header('Location: index.php');
 		exit;
 	}
