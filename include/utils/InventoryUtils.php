350dad743004c41828637555d8fc0c5c0850fdf8
satish <satish.dvnk@vtiger.com>
2016-08-31 12:34:15 +0530

    #291 - Into Products and Services multi-currencies is not loaded and dropped on save

--- include/utils/InventoryUtils.php
+++ include/utils/InventoryUtils.php
@@ -926,6 +926,8 @@ function getPriceDetailsForProduct($productid, $unit_price, $available='availabl
 			if ($currency_id == $product_currency_id) {
 				$is_basecurrency = true;
 			}
+
+			$price_details[$i]['check_value'] = true;
 			if ($cur_value == null || $cur_value == '') {
 				$price_details[$i]['check_value'] = false;
 				if	($unit_price != null) {
@@ -933,9 +935,8 @@ function getPriceDetailsForProduct($productid, $unit_price, $available='availabl
 				} else {
 					$cur_value = '0';
 				}
-			} else if($is_basecurrency){
-				$price_details[$i]['check_value'] = true;
 			}
+
 			$price_details[$i]['curvalue'] = CurrencyField::convertToUserFormat($cur_value, null, true);
 			$price_details[$i]['conversionrate'] = $actual_conversion_rate;
 			$price_details[$i]['is_basecurrency'] = $is_basecurrency;
