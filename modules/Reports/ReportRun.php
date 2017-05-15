c9f4c029466d893641d64e82a55963127f19bfed
its4you <sopko@its4you.sk>
2017-05-15 13:14:02 +0200

    back

--- modules/Reports/ReportRun.php
+++ modules/Reports/ReportRun.php
@@ -2848,9 +2848,6 @@ class ReportRun extends CRMEntity {
 		$advfiltersql = $this->getAdvFilterSql($reportid);
 
 		$this->totallist = $columnstotallist;
-		
-		$wheresql = "";
-		
 		global $current_user;
 		//Fix for ticket #4915.
 		$selectlist = $columnlist;
