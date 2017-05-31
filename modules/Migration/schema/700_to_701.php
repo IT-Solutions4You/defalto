e4c4611f27c471e31f8b7e751f51a31ef3124246
satish <satish.dvnk@vtiger.com>
2017-05-31 13:19:10 +0530

    Fixes #559 - Project Summary Broken

--- modules/Migration/schema/700_to_701.php
+++ modules/Migration/schema/700_to_701.php
@@ -31,4 +31,12 @@ if(defined('VTIGER_UPGRADE')) {
 				PRIMARY KEY (`scannerid`)
 			  ) ENGINE=InnoDB DEFAULT CHARSET=utf8", true);
 	}
+
+	$updateModulesList = array('Project' => 'packages/vtiger/optional/Projects.zip');
+	foreach ($updateModulesList as $moduleName => $packagePath) {
+		$moduleInstance = Vtiger_Module::getInstance($moduleName);
+		if($moduleInstance) {
+			updateVtlibModule($moduleName, $packagepath);
+		}
+	}
 }
