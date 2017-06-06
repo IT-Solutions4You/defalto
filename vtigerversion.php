af11e6d114977dc9f43e85230beca5308e7c6ae8
satish <satish.dvnk@vtiger.com>
2017-06-06 09:52:51 +0530

    Updated vtiger version for 7.0.1

--- vtigerversion.php
+++ vtigerversion.php
@@ -8,7 +8,7 @@
  * All Rights Reserved.
  ************************************************************************************/
 
-$patch_version = '20170605'; // -ve timestamp before release, +ve timestamp after release.
+$patch_version = '20170606'; // -ve timestamp before release, +ve timestamp after release.
 $modified_database = '';
 $vtiger_current_version = '7.0.1';
 $_SESSION['vtiger_version'] = $vtiger_current_version;
