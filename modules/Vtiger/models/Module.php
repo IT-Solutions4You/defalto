0e5c734ebd96e946b672e3bb52cf5299a15b3e05
prasad <prasad@vtiger.com>
2018-02-06 22:46:03 +0530

    Fixes #694: Safety check added to derive fallback related list fields

--- modules/Vtiger/models/Module.php
+++ modules/Vtiger/models/Module.php
@@ -593,17 +593,18 @@ class Vtiger_Module_Model extends Vtiger_Module {
 	 * @return <Array> returns related fields list.
 	 */
 	public function getRelatedListFields() {
-		$entityInstance = CRMEntity::getInstance($this->getName());
-		$list_fields_name = $entityInstance->list_fields_name;
-		$list_fields = $entityInstance->list_fields;
 		$relatedListFields = array();
-		foreach ($list_fields as $key => $fieldInfo) {
-			foreach ($fieldInfo as $columnName) {
-				if(array_key_exists($key, $list_fields_name)){
-					$relatedListFields[$columnName] = $list_fields_name[$key];
+		$entityInstance = CRMEntity::getInstance($this->getName());
+		if (isset($entityInstance->list_fields_name)) {
+			$list_fields_name = $entityInstance->list_fields_name;
+			$list_fields = $entityInstance->list_fields;
+			foreach ($list_fields as $key => $fieldInfo) {
+				foreach ($fieldInfo as $columnName) {
+					if(array_key_exists($key, $list_fields_name)){
+						$relatedListFields[$columnName] = $list_fields_name[$key];
+					}
 				}
 			}
-
 		}
 		return $relatedListFields;
 	}
