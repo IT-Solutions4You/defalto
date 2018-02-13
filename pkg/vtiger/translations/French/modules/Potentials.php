61b4002eacdd81801bc5389bee2c0cce11a8d520
adrien <adrien@Quartz-PC>
2018-02-13 11:51:00 +0100

    Potential

--- pkg/vtiger/translations/French/modules/Potentials.php
+++ pkg/vtiger/translations/French/modules/Potentials.php
@@ -8,56 +8,56 @@
  * All Rights Reserved.
  ************************************************************************************/
 $languageStrings = array(
-	'Potentials'                   => 'Affaires'                    , 
-	'SINGLE_Potentials'            => 'Affaire'                     , 
-	'LBL_ADD_RECORD'               => 'Add Opportunity'             , 
-	'LBL_RECORDS_LIST'             => 'Opportunities List'          , 
-	'LBL_OPPORTUNITY_INFORMATION'  => 'Détail'                     , 
-	'Potential No'                 => 'Affaire N°'                 , 
-	'Amount'                       => 'Montant'                     , 
-	'Next Step'                    => 'Suivant'                     , 
-	'Sales Stage'                  => 'Phase de vente'              , 
-	'Probability'                  => 'Probabilité'                , 
-	'Campaign Source'              => 'Campagne'                    , 
-	'Forecast Amount'              => 'Revenu pondéré'             , 
-	'Funnel'                       => 'Sales Funnel'                , 
-	'Potentials by Stage'          => 'Opportunities by Stage'      , 
-	'Total Revenue'                => 'Revenue by Salesperson'      , 
-	'Top Potentials'               => 'Top Opportunities'           , 
-	'Forecast'                     => 'Sales Forecast'              , 
-	'Prospecting'                  => 'Prospection'                 , 
-	'Qualification'                => 'Qualification '              , 
-	'Needs Analysis'               => 'A analyser'                  , 
-	'Value Proposition'            => 'Proposition'                 , 
-	'Id. Decision Makers'          => 'Ref décideurs'              , 
-	'Perception Analysis'          => 'Analyse de perception'       , 
-	'Proposal/Price Quote'         => 'Proposition/Prix'            , 
-	'Negotiation/Review'           => 'Négociation/Révision'      , 
-	'Closed Won'                   => 'Gagnée'                     , 
-	'Closed Lost'                  => 'Perdue'                      , 
-	'--None--'                     => '--Aucun--'                   , 
-	'Existing Business'            => 'Client existant'             , 
-	'New Business'                 => 'Nouveau client'              , 
-	'LBL_EXPECTED_CLOSE_DATE_ON'   => 'Expected to close on'        , 
-	'LBL_RELATED_CONTACTS'         => 'Related Contacts'            , // TODO: Review
-	'LBL_RELATED_PRODUCTS'         => 'Related Products'            , // TODO: Review
+    'Potentials'                   => 'Affaires'                    , 
+    'SINGLE_Potentials'            => 'Affaire'                     , 
+    'LBL_ADD_RECORD'               => 'Ajouter une Affaire'             , 
+    'LBL_RECORDS_LIST'             => 'Liste des Affaires'          , 
+    'LBL_OPPORTUNITY_INFORMATION'  => 'Détail'                     , 
+    'Potential No'                 => 'Affaire N°'                 , 
+    'Amount'                       => 'Montant'                     , 
+    'Next Step'                    => 'Suivant'                     , 
+    'Sales Stage'                  => 'Phase de vente'              , 
+    'Probability'                  => 'Probabilité'                , 
+    'Campaign Source'              => 'Campagne'                    , 
+    'Forecast Amount'              => 'Revenu pondéré'             , 
+    'Funnel'                       => 'Sales Funnel'                , 
+    'Potentials by Stage'          => 'Affaires par Etape'      , 
+    'Total Revenue'                => 'Revenue par Commercial'      , 
+    'Top Potentials'               => 'Top des Affaires'           , 
+    'Forecast'                     => 'Sales Forecast'              , 
+    'Prospecting'                  => 'Prospection'                 , 
+    'Qualification'                => 'Qualification '              , 
+    'Needs Analysis'               => 'A analyser'                  , 
+    'Value Proposition'            => 'Proposition'                 , 
+    'Id. Decision Makers'          => 'Ref décideurs'              , 
+    'Perception Analysis'          => 'Analyse de perception'       , 
+    'Proposal/Price Quote'         => 'Proposition/Prix'            , 
+    'Negotiation/Review'           => 'Négociation/Révision'      , 
+    'Closed Won'                   => 'Gagnée'                     , 
+    'Closed Lost'                  => 'Perdue'                      , 
+    '--None--'                     => '--Aucun--'                   , 
+    'Existing Business'            => 'Client existant'             , 
+    'New Business'                 => 'Nouveau client'              , 
+    'LBL_EXPECTED_CLOSE_DATE_ON'   => 'Expected to close on'        , 
+    'LBL_RELATED_CONTACTS'         => 'Contacts Liés'            , 
+    'LBL_RELATED_PRODUCTS'         => 'Produits Liés'            , 
     'Related To'                   => 'Nom de l\'organisation'      ,
     'Type'                         => 'Type'                        , 
-    
+
     //Convert Potentials
     'LBL_CONVERT_POTENTIAL' => 'Conversion nécessite la sélection du projet',
     'LBL_POTENTIALS_FIELD_MAPPING' => 'Possibilités de cartographie sur le terrain',
     'LBL_CONVERT_POTENTIALS_ERROR' => 'Vous devez activer le projet de convertir l\'opportunité',
     'LBL_POTENTIALS_FIELD_MAPPING_INCOMPLETE' => 'Opportunités mappage de champs est incomplète (Paramètres> Gestionnaire de modules> Perspectives> Perspectives mappage de champs)',
-	'LBL_CREATE_PROJECT'           => 'Créer le projet'            , 
-    
-    //Potentials Custom Field Mapping
-	'LBL_CUSTOM_FIELD_MAPPING'=> 'Possibilité de projet de cartographie',
+    'LBL_CREATE_PROJECT'           => 'Créer le projet'            , 
 
-  'Contact Name' => 'Nom Du Contact',
+    //Potentials Custom Field Mapping
+    'LBL_CUSTOM_FIELD_MAPPING'=> 'Possibilité de projet de cartographie',
+
+    'Contact Name' => 'Nom Du Contact',
 
 );
 
 $jsLanguageStrings = array(
-	'JS_SELECT_PROJECT_TO_CONVERT_LEAD' => 'Conversion nécessite la sélection du projet',
+    'JS_SELECT_PROJECT_TO_CONVERT_LEAD' => 'Conversion nécessite la sélection du projet',
 );
