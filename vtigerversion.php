2022c6c327cd58e358c909949789998cbe3d2f4d
prasad <prasad@vtiger.com>
2018-03-07 12:51:02 +0530

    Moved patch version marker ahead

--- vtigerversion.php
+++ vtigerversion.php
@@ -8,7 +8,7 @@
  * All Rights Reserved.
  ************************************************************************************/
 
-$patch_version = '20180304'; // -ve timestamp before release, +ve timestamp after release.
+$patch_version = '20180305'; // -ve timestamp before release, +ve timestamp after release.
 $modified_database = '';
 $vtiger_current_version = '7.1.0';
 $_SESSION['vtiger_version'] = $vtiger_current_version;
