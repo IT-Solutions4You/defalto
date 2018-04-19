c55befef0dddc9c5be732ea27cccaf4afc321098
prasad <prasad@vtiger.com>
2018-04-19 09:10:01 +0530

    Advancing to next version

--- vtigerversion.php
+++ vtigerversion.php
@@ -10,7 +10,7 @@
 
 $patch_version = '20180308'; // -ve timestamp before release, +ve timestamp after release.
 $modified_database = '';
-$vtiger_current_version = '7.1.0';
+$vtiger_current_version = '7.1.1';
 $_SESSION['vtiger_version'] = $vtiger_current_version;
 
 ?>
