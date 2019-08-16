dd480fa93de5772a6250aafea0f32b74ab9aa3a4
Uma <uma.s@vtiger.com>
2019-08-16 13:13:49 +0530

    Reverted change on master

--- modules/Vtiger/views/ShowWidget.php
+++ modules/Vtiger/views/ShowWidget.php
@@ -10,14 +10,8 @@
 
 class Vtiger_ShowWidget_View extends Vtiger_IndexAjax_View {
 
-	public function requiresPermission(Vtiger_Request $request){
-		$permissions = parent::requiresPermission($request);
-		if($request->get('module') != 'Dashboard'){
-			$request->set('custom_module', 'Dashboard');
-			$permissions[] = array('module_parameter' => 'custom_module', 'action' => 'DetailView');
-		}
-		
-		return $permissions;
+	function checkPermission(Vtiger_Request $request) {
+		return true;
 	}
 
 	function process(Vtiger_Request $request) {
