16003545421c073b78c2504d0a70237fb5ac08ed
Uma <uma.s@vtiger.com>
2019-12-09 16:27:12 +0530

    #1198::Uma::FieldBasic update api message correction

--- vtlib/Vtiger/FieldBasic.php
+++ vtlib/Vtiger/FieldBasic.php
@@ -211,7 +211,7 @@ class Vtiger_FieldBasic {
 	 * @internal TODO
 	 */
 	function __update() {
-		self::log("Make use of Vtiger_Field_Model __update() api ...");
+		self::log("Make use of Vtiger_Field_Model => __update() api.");
 	}
 
 	/**
