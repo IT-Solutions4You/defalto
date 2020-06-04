659c7b9a89fe361a9fa7873d39c7b158fc94da02
Daniel Voelskow <daniel.voelskow@interface-projects.de>
2020-06-04 08:56:41 +0200

    #1296 Fix php72 warning on campaigns

--- modules/Campaigns/Campaigns.php
+++ modules/Campaigns/Campaigns.php
@@ -171,7 +171,7 @@ class Campaigns extends CRMEntity {
 
 		if($return_value == null)
 			$return_value = Array();
-		else if($is_CampaignStatusAllowed) {
+		else if($is_CampaignStatusAllowed && is_array($return_value['header'])) {
 			$statusPos = count($return_value['header']) - 2; // Last column is for Actions, exclude that. Also the index starts from 0, so reduce one more count.
 			$return_value = $this->add_status_popup($return_value, $statusPos, 'Accounts');
 		}
