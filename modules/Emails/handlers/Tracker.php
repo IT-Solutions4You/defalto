3a46958af6f3ac2b8a9d816a11c17bf9dba47e07
Uma <uma.s@vtiger.com>
2020-06-16 20:19:32 +0530

    Fixes Email tracking vulnerability has been addressed

--- modules/Emails/handlers/Tracker.php
+++ modules/Emails/handlers/Tracker.php
@@ -1,8 +1,12 @@
 <?php
-/**
- * Copyright (C) www.vtiger.com. All rights reserved.
- * @license Vtiger Commercial License 1.1. Reverse engineering restricted.
- */
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.1
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
 
 include_once 'modules/Users/Users.php';
 require_once 'include/events/include.inc';
