f0d76b0572cfe35d55d5ddcd8e08edd40783b5a7
Uma <uma.s@vtiger.com>
2020-08-07 18:40:33 +0530

    Fixes #1438 Textarea fields data save

--- modules/Vtiger/uitypes/Text.php
+++ modules/Vtiger/uitypes/Text.php
@@ -16,14 +16,10 @@ class Vtiger_Text_UIType extends Vtiger_Base_UIType {
 	 * @return <Object>
 	 */
 	public function getDisplayValue($value, $record=false, $recordInstance = false,$removeTags = false) {
-		//This API replaces newlines to html br tags, and spaces with &nbsp;
-		// It should not replace spaces within html tags
-		$value = decode_html(preg_replace('/\r\n|\r|\n|&NewLine;|&amp;NewLine;/','<br>',$value));
-		if($removeTags){
-            $value = strip_tags($value,'<br>');
-        }
-		$value = purifyHtmlEventAttributes($value, true);
-		return $value;
+		if(in_array($this->get('field')->getFieldName(),array('signature','commentcontent'))) {
+			return $value;
+		}
+		return nl2br(purifyHtmlEventAttributes($value, true));
 	}
     
     /**
