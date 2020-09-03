510dfd58d81090963c481223f63b150dd9f8ce6c
Uma <uma.s@vtiger.com>
2020-09-03 11:49:16 +0530

    Fixes Add tags from MyPreference

--- modules/Settings/Tags/views/EditAjax.php
+++ modules/Settings/Tags/views/EditAjax.php
@@ -10,6 +10,10 @@
 
 class Settings_Tags_EditAjax_View extends Settings_Vtiger_IndexAjax_View {
 
+        function checkPermission(Vtiger_Request $request) {
+            return true;
+	}
+        
 	public function process(Vtiger_Request $request) {
 		$viewer = $this->getViewer($request);
 		$qualifiedName = $request->getModule(false);
