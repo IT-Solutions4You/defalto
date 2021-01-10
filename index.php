ee43f820ece0489e777a56bc7d41a349fe3dc814
Uma <uma.s@vtiger.com>
2021-01-10 19:01:27 +0530

    remove wrong debugging

--- index.php
+++ index.php
@@ -16,5 +16,6 @@ include_once 'include/Webservices/Relation.php';
 include_once 'vtlib/Vtiger/Module.php';
 include_once 'includes/main/WebUI.php';
 
+print('test here');
 $webUI = new Vtiger_WebUI();
 $webUI->process(new Vtiger_Request($_REQUEST, $_REQUEST));
