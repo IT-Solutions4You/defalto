13d84d8dfbf7a324b6c6e320e1dc6aef8a4f8d1d
Uma <uma.s@vtiger.com>
2021-07-15 20:27:11 +0530

    Version upgraded to GA Release

--- vtigerversion.php
+++ vtigerversion.php
@@ -8,9 +8,9 @@
  * All Rights Reserved.
  ************************************************************************************/
 
-$patch_version = '20210710'; // -ve timestamp before release, +ve timestamp after release.
+$patch_version = '20210716'; // -ve timestamp before release, +ve timestamp after release.
 $modified_database = '';
-$vtiger_current_version = '7.4.0RC';
+$vtiger_current_version = '7.4.0';
 $_SESSION['vtiger_version'] = $vtiger_current_version;
 
 ?>
