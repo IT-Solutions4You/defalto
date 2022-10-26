fe3916aa0c29b732d109955c14a03f3f2a4ec8db
Prasad <prasad@vtiger.com>
2022-10-26 18:38:09 +0530

    Fixed data-type in Smarty exception calss

--- libraries/Smarty/libs/sysplugins/smartycompilerexception.php
+++ libraries/Smarty/libs/sysplugins/smartycompilerexception.php
@@ -20,7 +20,7 @@ class SmartyCompilerException extends SmartyException
      *
      * @type int|null
      */
-    public $line = null;
+    public int $line = 0;
 
     /**
      * The template source snippet relating to the error
