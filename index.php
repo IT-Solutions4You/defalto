5c431d52f5561996095027d543675048d7f5c8a9
Martin Jurčišin <jurcisin@its4you.sk>
2022-12-09 15:46:00 +0100

    HOTFIX: adjustments to composer

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-/*+**********************************************************************************
+/***********************************************************************************
  * The contents of this file are subject to the vtiger CRM Public License Version 1.1
  * ("License"); You may not use this file except in compliance with the License
  * The Original Code is:  vtiger CRM Open Source
@@ -11,7 +11,8 @@
 //Overrides GetRelatedList : used to get related query
 //TODO : Eliminate below hacking solution
 include_once 'config.php';
-require_once('vendor/autoload.php');
+require_once 'vendorCheck.php';
+require_once 'vendor/autoload.php';
 include_once 'include/Webservices/Relation.php';
 
 include_once 'vtlib/Vtiger/Module.php';
