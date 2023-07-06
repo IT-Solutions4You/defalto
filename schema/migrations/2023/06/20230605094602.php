4341d9eacc70aa4314e994a382b6a23ee624505b
martin.jurcisin <M4rtuska>
2023-07-06 10:13:30 +0200

    Feature 36: Change the behavior of Currency UI type

--- schema/migrations/2023/06/20230605094602.php
+++ schema/migrations/2023/06/20230605094602.php
@@ -16,13 +16,41 @@ if (!class_exists('Migration_20230605094602')) {
          */
         public function migrate(string $strFileName): void
         {
-            $sql = 'SELECT DISTINCT vtiger_field.tabid, vtiger_entityname.tablename 
+            $sql = 'SELECT DISTINCT vtiger_field.tabid, vtiger_field.block, vtiger_field.sequence, vtiger_entityname.tablename, vtiger_entityname.modulename 
                     FROM vtiger_field 
-                    INNER JOIN vtiger_entityname ON vtiger_entityname.tabid = vtiger_field.tabid
+                        INNER JOIN vtiger_entityname ON vtiger_entityname.tabid = vtiger_field.tabid
                     WHERE uitype = 71';
             $tabIdResult = $this->db->query($sql);
 
             while ($tabIdRow = $this->db->fetchByAssoc($tabIdResult)) {
+                $module = Vtiger_Module::getInstance($tabIdRow['modulename']);
+
+                if ($module) {
+                    $field = Vtiger_Field::getInstance('currency_id', $module);
+
+                    if (!$field) {
+                        $block = Vtiger_Block::getInstance($tabIdRow['block'], $module);
+
+                        if ($block) {
+                            $currencyId = new Vtiger_Field();
+                            $currencyId->table = $tabIdRow['tablename'];
+                            $currencyId->name = 'currency_id';
+                            $currencyId->column = 'currency_id';
+                            $currencyId->label = 'Currency';
+                            $currencyId->uitype = 117;
+                            $currencyId->presence = 0;
+                            $currencyId->sequence = (int)$tabIdRow['sequence'] + 1;
+                            $currencyId->columntype = 'INT(19)';
+                            $currencyId->typeofdata = 'I~O';
+                            $currencyId->quickcreate = 3;
+                            $currencyId->masseditable = 1;
+                            $currencyId->summaryfield = 0;
+                            $currencyId->displaytype = 1;
+                            $currencyId->save($block);
+                        }
+                    }
+                }
+
                 $tableName = $tabIdRow['tablename'];
                 Vtiger_Utils::AddColumn($tableName, 'currency_id', 'INT(19)');
                 Vtiger_Utils::AddColumn($tableName, 'conversion_rate', 'DECIMAL(10,3)');
