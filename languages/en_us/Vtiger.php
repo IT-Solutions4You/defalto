701f18255f0a827c650225b35f351cc4afe516dd
Martin Jurcisin <jurcisin@its4you.sk>
2024-02-10 21:25:47 +0100

    security::XSS: In Dashboard Name

--- languages/en_us/Vtiger.php
+++ languages/en_us/Vtiger.php
@@ -1417,6 +1417,7 @@ $languageStrings = array(
     'LBL_RECORD_READONLY' => 'The record you are trying to edit is readonly.',
     'LBL_MAKE_EDITABLE' => 'Make Editable',
     'LBL_MAKE_READONLY' => 'Make Readonly',
+    'LBL_DASHBOARD_TAB_INVALID' => 'Invalid Tab Name ',
 );
 
 $jsLanguageStrings = array(
