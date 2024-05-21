d2c35a0caa57300f6e7f7f197acc3f77ecd2fa83
Martin Jurcisin <jurcisin@its4you.sk>
2024-05-21 21:18:01 +0200

    Add features from vtiger 8.2

--- modules/Settings/LayoutEditor/actions/Field.php
+++ modules/Settings/LayoutEditor/actions/Field.php
@@ -1,13 +1,10 @@
 <?php
-
-/*+**********************************************************************************
- * The contents of this file are subject to the vtiger CRM Public License Version 1.1
- * ("License"); You may not use this file except in compliance with the License
- * The Original Code is:  vtiger CRM Open Source
+/**
  * The Initial Developer of the Original Code is vtiger.
- * Portions created by vtiger are Copyright (C) vtiger.
+ * Portions created by vtiger are Copyright (c) vtiger.
+ * Portions created by IT-Solutions4You (ITS4You) are Copyright (c) IT-Solutions4You s.r.o
  * All Rights Reserved.
- ************************************************************************************/
+ */
 
 class Settings_LayoutEditor_Field_Action extends Settings_Vtiger_Index_Action {
 
