0336913cee814bf95ad2d1e0318a84108e63b7e3
Martin Jurcisin <jurcisin@its4you.sk>
2024-07-09 16:44:43 +0200

    #89 Inventory Items

--- schema/migrations/2024/06/20240619081559.php
+++ schema/migrations/2024/06/20240619081559.php
@@ -33,7 +33,7 @@ if (!class_exists('Migration_20240619081559')) {
                 $entriesRes = $this->db->pquery('SELECT crmid, smownerid FROM vtiger_crmentity WHERE setype = ? AND deleted = 0', [$inventoryModule]);
 
                 while ($entry = $this->db->fetch_array($entriesRes)) {
-                    $productsRes = $this->db->pquery('SELECT * FROM vtiger_inventoryproductrel WHERE crmid = ?', [$entry['crmid']]);
+                    $productsRes = $this->db->pquery('SELECT * FROM vtiger_inventoryproductrel WHERE id = ?', [$entry['crmid']]);
 
                     while ($productsRow = $this->db->fetchByAssoc($productsRes)) {
                         $inventoryItem = CRMEntity::getInstance('InventoryItem');
@@ -45,19 +45,40 @@ if (!class_exists('Migration_20240619081559')) {
                         $inventoryItem->column_fields['price'] = $productsRow['listprice'];
                         $inventoryItem->column_fields['subtotal'] = (float)$productsRow['listprice'] * (float)$productsRow['quantity'];
                         $inventoryItem->column_fields['discount'] = $productsRow['discount_percent'];
-                        $inventoryItem->column_fields['discount_amount'] = $productsRow['discount_amount'];
+
+                        if (!empty($productsRow['discount_amount'])) {
+                            $inventoryItem->column_fields['discount_amount'] = $productsRow['discount_amount'];
+                        } elseif (!empty($productsRow['discount_percent'])) {
+                            $inventoryItem->column_fields['discount_amount'] = $inventoryItem->column_fields['subtotal'] * $productsRow['discount_percent'] / 100;
+                        } else {
+                            $inventoryItem->column_fields['discount_amount'] = 0;
+                        }
+
+                        $inventoryItem->column_fields['total_after_discount'] = $inventoryItem->column_fields['subtotal'] - $inventoryItem->column_fields['discount_amount'];
                         $inventoryItem->column_fields['description'] = $productsRow['comment'];
                         $inventoryItem->column_fields['purchase_cost'] = $productsRow['purchase_cost'];
 
+                        $productServiceEntityType = getSalesEntityType($productsRow['productid']);
+                        $productServiceFocus = CRMEntity::getInstance($productServiceEntityType);
+                        $productServiceFocus->retrieve_entity_info($productsRow['productid'], $productServiceEntityType);
+                        $inventoryItem->column_fields['item_text'] = $productServiceFocus->column_fields['productname'];
+
+                        switch ($productServiceEntityType) {
+                            case 'Services':
+                                $inventoryItem->column_fields['unit'] = $productServiceFocus->column_fields['service_usageunit'];
+                                break;
+                            default:
+                                $inventoryItem->column_fields['unit'] = $productServiceFocus->column_fields['usageunit'];
+                                break;
+                        }
+
                         /**
-                         * item_text potrebujem poriesit z focusu Produktu/sluzby, taktiez aj unit
-                         * Discount a Discount amount sice prenasam, ale potrebujem si Discount amount vzdy vycislit a spocitat a ulozit aj cenu po zlave, teda total_after_discount
                          * V pripade, ze je zadana v ponuke/objed/fakture celkova zlava, tak z nej vycislujem overall discount pri polozkach a teda naplnam overall_discount, overall_discount_amount a total_after_overall_discount
-                         * Ak aj nie je celkova zlava, tak total_after_overall_discount musim naplnit tym co je v total_after_discount
+                         * Ak je Cekova zlava percento, tak dat spravne percenta; ak je celkova suma, tak to hodit do halieroveho vyrovnania
+                         * Ak aj nie je celkova zlava, tak total_after_overall_discount musim naplnit tym co je v total_after_discount:
                          * Tiez musim zistit ake su v systeme dane, zistit si spravne nazvy stlpcov, spocitat si ich, na zaklade toto naplnit polia tax a tax amount
                          * Nakoniec vycislujem Total - to je vlastne cena po dani
                          */
-                        //$inventoryItem->column_fields['item_text'] = getEntityName(getSalesEntityType($productsRow['productid']), $productsRow['productid'])[$productsRow['productid']];
 
                     }
                 }
