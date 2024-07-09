1ed5a89ce40fa45608f84213c9d8d6eee6ff16ff
Errorsupport <frantisekmichlik3012@gmail.com>
2024-07-09 08:55:36 +0200

    Feature #106 - Calendar setting quick edit

--- layouts/d1/modules/Users/resources/Field.js
+++ layouts/d1/modules/Users/resources/Field.js
@@ -4,8 +4,10 @@
 * Portions created by IT-Solutions4You (ITS4You) are Copyright (c) IT-Solutions4You s.r.o
 * All Rights Reserved.
 */
-Vtiger_Field_Js("Users_Field_Js",{},{})
+/** @var Users_Field_Js */
+Vtiger_Field_Js('Users_Field_Js',{},{})
 
+/** @var Users_Picklist_Field_Js */
 Vtiger_Field_Js('Users_Picklist_Field_Js',{},{
 
 	/**
@@ -20,34 +22,49 @@ Vtiger_Field_Js('Users_Picklist_Field_Js',{},{
 	 * Function to get the ui
 	 * @return - select element and chosen element
 	 */
-	getUi : function() {
-        //added class inlinewidth
-		var html = '<select  class="select2 inputElement inlinewidth" name="'+ this.getName() +'" id="field_'+this.getModuleName()+'_'+this.getName()+'">';
-		var pickListValues = this.getPickListValues();
-		var selectedOption = app.htmlDecode(this.getValue());
-        if(jQuery.trim(selectedOption).length === 0) {
+	getUi: function () {
+		let html = '<select  class="select2 inputElement inlinewidth" data-width="80%" name="' + this.getName() + '" id="field_' + this.getModuleName() + '_' + this.getName() + '">',
+			pickListValues = this.getPickListValues(),
+			selectedOption = app.htmlDecode(this.getValue());
+
+		if (jQuery.trim(selectedOption).length === 0) {
 			selectedOption = '&nbsp;';
 		}
+
 		html += '';
-		for(var option in pickListValues) {
-			if(jQuery.trim(option).length === 0) {
+
+		for (let option in pickListValues) {
+			if (jQuery.trim(option).length === 0) {
 				option = '&nbsp;';
 			}
-			
-			html += '<option value="'+option+'" ';
-			
-			if(option == selectedOption) {
+
+			html += '<option value="' + option + '" ';
+
+			if (option == selectedOption) {
 				html += ' selected ';
 			}
-			
-			if(option == '&nbsp;' && (this.getName() == 'currency_decimal_separator' || this.getName() == 'currency_grouping_separator')) {
-				html += '>'+app.vtranslate('Space')+'</option>';
+
+			if (option == '&nbsp;' && (this.getName() == 'currency_decimal_separator' || this.getName() == 'currency_grouping_separator')) {
+				html += '>' + app.vtranslate('Space') + '</option>';
 			}
-			html += '>'+pickListValues[option]+'</option>';
+
+			html += '>' + pickListValues[option] + '</option>';
 		}
-		html +='</select>';
-		var selectContainer = jQuery(html);
-		this.addValidationToElement(selectContainer);
-		return selectContainer;
+
+		html += '</select>';
+
+		return this.addValidationToElement(jQuery(html));
+	}
+});
+
+/** @var Users_Multipicklist_Field_Js */
+Vtiger_Multipicklist_Field_Js('Users_Multipicklist_Field_Js',{},{
+
+	getUi: function () {
+		let html = this._super()
+
+		html.data('width', '80%');
+
+		return html;
 	}
 });
