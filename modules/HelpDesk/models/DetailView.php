50ca8addf262f1769ea4c56f78d0a3dec9b7040f
Errorsupport <frantisekmichlik3012@gmail.com>
2024-07-29 13:22:48 +0200

    Feature #119 - Remove duplicate summary widget Documents from HelpDesk, Potentials, Project

--- modules/HelpDesk/models/DetailView.php
+++ modules/HelpDesk/models/DetailView.php
@@ -35,36 +35,4 @@ class HelpDesk_DetailView_Model extends Vtiger_DetailView_Model {
 
 		return $linkModelList;
 	}
-
-	/**
-	 * Function to get the detail view widgets
-	 * @return <Array> - List of widgets , where each widget is an Vtiger_Link_Model
-	 */
-	public function getWidgets() {
-		$userPrivilegesModel = Users_Privileges_Model::getCurrentUserPrivilegesModel();
-		$widgetLinks = parent::getWidgets();
-		$widgets = array();
-
-        $documentsInstance = Vtiger_Module_Model::getInstance('Documents');
-		if($userPrivilegesModel->hasModuleActionPermission($documentsInstance->getId(), 'DetailView')) {
-			$createPermission = $userPrivilegesModel->hasModuleActionPermission($documentsInstance->getId(), 'CreateView');
-			$widgets[] = array(
-					'linktype' => 'DETAILVIEWWIDGET',
-					'linklabel' => 'Documents',
-					'linkName'	=> $documentsInstance->getName(),
-					'linkurl' => 'module='.$this->getModuleName().'&view=Detail&record='.$this->getRecord()->getId().
-							'&relatedModule=Documents&mode=showRelatedRecords&page=1&limit=5',
-					'action'	=>	($createPermission == true) ? array('Add') : array(),
-					'actionURL' =>	$documentsInstance->getQuickCreateUrl()
-			);
-		}
-
-		foreach ($widgets as $widgetDetails) {
-			$widgetLinks[] = Vtiger_Link_Model::getInstanceFromValues($widgetDetails);
-		}
-
-		return $widgetLinks;
-	}
-
-
 }
--- modules/Potentials/models/DetailView.php
+++ modules/Potentials/models/DetailView.php
@@ -77,20 +77,6 @@ class Potentials_DetailView_Model extends Vtiger_DetailView_Model {
 		$widgetLinks = parent::getWidgets();
 		$widgets = array();
 
-		$documentsInstance = Vtiger_Module_Model::getInstance('Documents');
-		if($userPrivilegesModel->hasModuleActionPermission($documentsInstance->getId(), 'DetailView')) {
-			$createPermission = $userPrivilegesModel->hasModuleActionPermission($documentsInstance->getId(), 'CreateView');
-			$widgets[] = array(
-					'linktype' => 'DETAILVIEWWIDGET',
-					'linklabel' => 'Documents',
-					'linkName'	=> $documentsInstance->getName(),
-					'linkurl' => 'module='.$this->getModuleName().'&view=Detail&record='.$this->getRecord()->getId().
-							'&relatedModule=Documents&mode=showRelatedRecords&page=1&limit=5',
-					'action'	=>	($createPermission == true) ? array('Add') : array(),
-					'actionURL' =>	$documentsInstance->getQuickCreateUrl()
-			);
-		}
-
 		$contactsInstance = Vtiger_Module_Model::getInstance('Contacts');
 		if($userPrivilegesModel->hasModuleActionPermission($contactsInstance->getId(), 'DetailView')) {
 			$createPermission = $userPrivilegesModel->hasModuleActionPermission($contactsInstance->getId(), 'CreateView');
