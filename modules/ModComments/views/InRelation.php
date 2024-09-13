b7a0328fbd57a78f5e3c2cfbad40ec89c68cede3
Errorsupport <errors.pport@gmail.com>
2024-09-13 16:23:40 +0200

    #136 Show all comments support "startIndex" change from request

--- modules/ModComments/views/InRelation.php
+++ modules/ModComments/views/InRelation.php
@@ -10,49 +10,50 @@
 
 class ModComments_InRelation_View extends Vtiger_RelatedList_View {
 
-	function process(Vtiger_Request $request) {
-		$startindex = 0;
-		$parentRecordId = $request->get('record');
-		$commentRecordId = $request->get('commentid');
-		$moduleName = $request->getModule();
-		$rollupSettings = $request->get('rollup_settings');
-		$rollupStatus = isset($rollupSettings['rollup_status'])? $rollupSettings['rollup_status'] : false;
-		$rollupid = isset($rollupSettings['rollupid']) ? $rollupSettings['rollupid'] : false;
-		$currentUserModel = Users_Record_Model::getCurrentUserModel();
-		$modCommentsModel = Vtiger_Module_Model::getInstance('ModComments');
+    public function process(Vtiger_Request $request)
+    {
+        $startIndex = $request->get('startIndex', 0);
+        $parentRecordId = $request->get('record');
+        $commentRecordId = $request->get('commentid');
+        $moduleName = $request->getModule();
+        $rollupSettings = $request->get('rollup_settings');
+        $rollupStatus = isset($rollupSettings['rollup_status']) ? $rollupSettings['rollup_status'] : false;
+        $rollupid = isset($rollupSettings['rollupid']) ? $rollupSettings['rollupid'] : false;
+        $currentUserModel = Users_Record_Model::getCurrentUserModel();
+        $modCommentsModel = Vtiger_Module_Model::getInstance('ModComments');
 
-		if ($rollupStatus) {
-			$parentRecordModel = Vtiger_Record_Model::getInstanceById($parentRecordId, $moduleName);
-			$parentCommentModels = $parentRecordModel->getRollupCommentsForModule($startindex);
-			$startindex = $startindex + 10;
-		} else {
-			$parentCommentModels = ModComments_Record_Model::getAllParentComments($parentRecordId);
-		}
+        if ($rollupStatus) {
+            $parentRecordModel = Vtiger_Record_Model::getInstanceById($parentRecordId, $moduleName);
+            $parentCommentModels = $parentRecordModel->getRollupCommentsForModule($startIndex);
+            $startIndex = $startIndex + 10;
+        } else {
+            $parentCommentModels = ModComments_Record_Model::getAllParentComments($parentRecordId);
+        }
 
-		$currentCommentModel = null;
-		if (!empty($commentRecordId)) {
-			$currentCommentModel = ModComments_Record_Model::getInstanceById($commentRecordId);
-		}
+        $currentCommentModel = null;
+        if (!empty($commentRecordId)) {
+            $currentCommentModel = ModComments_Record_Model::getInstanceById($commentRecordId);
+        }
 
-		// To get field model of filename
-		$fileNameFieldModel = Vtiger_Field::getInstance("filename", $modCommentsModel);
-		$fileFieldModel = Vtiger_Field_Model::getInstanceFromFieldObject($fileNameFieldModel);
-		$viewer = $this->getViewer($request);
-		$viewer->assign('CURRENTUSER', $currentUserModel);
-		$viewer->assign('COMMENTS_MODULE_MODEL', $modCommentsModel);
-		$viewer->assign('PARENT_COMMENTS', $parentCommentModels);
-		$viewer->assign('CURRENT_COMMENT', $currentCommentModel);
-		$viewer->assign('FIELD_MODEL', $fileFieldModel);
-		$viewer->assign('MAX_UPLOAD_LIMIT_MB', Vtiger_Util_Helper::getMaxUploadSize());
-		$viewer->assign('MAX_UPLOAD_LIMIT_BYTES', Vtiger_Util_Helper::getMaxUploadSizeInBytes());
+        // To get field model of filename
+        $fileNameFieldModel = Vtiger_Field::getInstance("filename", $modCommentsModel);
+        $fileFieldModel = Vtiger_Field_Model::getInstanceFromFieldObject($fileNameFieldModel);
+        $viewer = $this->getViewer($request);
+        $viewer->assign('CURRENTUSER', $currentUserModel);
+        $viewer->assign('COMMENTS_MODULE_MODEL', $modCommentsModel);
+        $viewer->assign('PARENT_COMMENTS', $parentCommentModels);
+        $viewer->assign('CURRENT_COMMENT', $currentCommentModel);
+        $viewer->assign('FIELD_MODEL', $fileFieldModel);
+        $viewer->assign('MAX_UPLOAD_LIMIT_MB', Vtiger_Util_Helper::getMaxUploadSize());
+        $viewer->assign('MAX_UPLOAD_LIMIT_BYTES', Vtiger_Util_Helper::getMaxUploadSizeInBytes());
 
-		$viewer->assign('MODULE_NAME', $request->getModule());
-		$viewer->assign('MODULE_RECORD', $parentRecordId);
-		$viewer->assign('ROLLUP_STATUS', $rollupStatus);
-		$viewer->assign('ROLLUPID', $rollupid);
-		$viewer->assign('STARTINDEX', $startindex);
+        $viewer->assign('MODULE_NAME', $request->getModule());
+        $viewer->assign('MODULE_RECORD', $parentRecordId);
+        $viewer->assign('ROLLUP_STATUS', $rollupStatus);
+        $viewer->assign('ROLLUPID', $rollupid);
+        $viewer->assign('STARTINDEX', $startIndex);
+        $viewer->assign('PARENT_RECORD', $parentRecordId);
 
-		return $viewer->view('ShowAllComments.tpl', $moduleName, 'true');
-	}
-
+        $viewer->view('ShowAllComments.tpl', $moduleName);
+    }
 }
