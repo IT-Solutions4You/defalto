39b1555d3aa7e339c1938bf4064c06f1713c97db
Errorsupport <errors.pport@gmail.com>
2024-09-16 14:32:45 +0200

    #139 Fix function checkRecordExistance

--- modules/Home/models/Module.php
+++ modules/Home/models/Module.php
@@ -143,7 +143,7 @@ class Home_Module_Model extends Vtiger_Module_Model {
 			$moduleName = $row['module'];
 			$recordId = $row['crmid'];
 
-			if(Users_Privileges_Model::isPermitted($moduleName, 'DetailView', $recordId) && isRecordExists($recordId)){
+			if(Users_Privileges_Model::isPermitted($moduleName, 'DetailView', $recordId)){
 				$modTrackerRecorModel = new ModTracker_Record_Model();
 				$modTrackerRecorModel->setData($row)->setParent($recordId, $moduleName);
 				$activites[] = $modTrackerRecorModel;
