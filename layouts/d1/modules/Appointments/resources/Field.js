c7c77583beacc93310af6a59d97962ab7710de60
Errorsupport <errors.pport@gmail.com>
2024-10-14 14:41:00 +0200

    Appointments edit view save (#150)
    * #149 Appointments edit view and save action fixes
    
    * #149 Alert in QueryGenerator for languages and added Appointments language
    
    * #150 Version change
    
    * #149 Format code for appointments

--- layouts/d1/modules/Appointments/resources/Field.js
+++ layouts/d1/modules/Appointments/resources/Field.js
@@ -1,7 +1,7 @@
 jQuery.validator.addMethod('reminder_required', function (value, element, params) {
     let selectionElement = $(element).parents('#js-reminder-selections')
 
-    return parseIntselectionElement.find('#js-reminder-days').val() ||
+    return selectionElement.find('#js-reminder-days').val() ||
         selectionElement.find('#js-reminder-hours').val() ||
         selectionElement.find('#js-reminder-minutes').val();
 }, jQuery.validator.format(app.vtranslate('JS_REQUIRED_TIME_MORE_THAN_ZERO')));
