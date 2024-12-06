94a9f584cf804560410e9bd4a3124b8cfc889de4
Errorsupport <errors.pport@gmail.com>
2024-12-06 10:40:24 +0100

    #158 - Install model skip fields update column in update/migration process

--- modules/Vtiger/models/Field.php
+++ modules/Vtiger/models/Field.php
@@ -1598,4 +1598,12 @@ class Vtiger_Field_Model extends Vtiger_Field {
     {
         return (int)$this->get('uitype');
     }
+
+    /**
+     * @return Core_DatabaseData_Model
+     */
+    public function getFieldTable(): Core_DatabaseData_Model
+    {
+        return (new Core_DatabaseData_Model())->getTable('vtiger_field', 'fieldid');
+    }
 }
