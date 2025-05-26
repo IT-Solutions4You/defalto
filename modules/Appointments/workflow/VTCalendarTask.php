78524422391c08ed8fbc91320d0c356b24ec83f7
Errorsupport <errors.pport@gmail.com>
2025-05-26 11:47:10 +0200

    #168 - Appointments calendar task moved from workflows to workflow folder

--- modules/Appointments/workflow/VTCalendarTask.php
+++ modules/Appointments/workflow/VTCalendarTask.php
@@ -7,6 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+require_once 'modules/com_vtiger_workflow/VTTaskManager.inc';
 require_once 'modules/com_vtiger_workflow/VTEntityCache.inc';
 require_once 'modules/com_vtiger_workflow/VTWorkflowUtils.php';
 require_once 'modules/com_vtiger_workflow/VTSimpleTemplate.inc';
