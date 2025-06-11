c8cfd65867c2280fb17cb5841d66ce6433767b8b
Errorsupport <errors.pport@gmail.com>
2025-06-11 09:47:58 +0200

    #168 - Installer skip folders and files on install

--- modules/Installer/Installer.php
+++ modules/Installer/Installer.php
@@ -1,8 +1,5 @@
 <?php
 
-Installer_ZipArchive_Model::$skipFolders = ['user_privileges', 'manifest', 'update', 'icons', 'installer'];
-Installer_ZipArchive_Model::$skipFiles = ['config.inc.php', 'composer.lock', 'index.php', 'update.php', 'install.php', 'parent_tabdata.php', 'tabdata.php'];
-
 class Installer extends CRMExtension
 {
     public string $moduleLabel = 'Installer';
