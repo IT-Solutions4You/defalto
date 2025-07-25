1941665b0d0eea4c0e40ffb1ad52e65a7e292b74
Errorsupport <errors.pport@gmail.com>
2025-07-25 08:24:49 +0200

    #202 - Vtiger install process added table crm entity

--- modules/Vtiger/models/Install.php
+++ modules/Vtiger/models/Install.php
@@ -30,6 +30,10 @@ class Vtiger_Install_Model extends Core_Install_Model {
 
     public function installModule()
     {
+        if ($this->isRequiredInstallTables()) {
+            $this->installTables();
+        }
+
         (new Vtiger_Field_Model())->insertDefaultData();
     }
 
@@ -41,8 +45,20 @@ class Vtiger_Install_Model extends Core_Install_Model {
         (new Vtiger_Module_Model())->createTables();
         (new Vtiger_Field_Model())->createTables();
         (new Vtiger_Block_Model())->createTables();
+        (new Vtiger_Record_Model())->createTables();
         (new Core_BlockUiType_Model())->createTables();
         (new Settings_Workflows_Record_Model())->createTables();
         (new Settings_Workflows_TaskRecord_Model())->createTables();
+
+        $this->createPicklistTable('vtiger_taxtype', 'taxtypeid', 'taxtype');
+    }
+
+    public function migrate()
+    {
+        $fieldTable = (new Vtiger_Field_Model())->getFieldTable();
+        $fieldTable->updateData(['columnname' => 'assigned_user_id', 'fieldname' => 'assigned_user_id'], ['columnname' => 'smownerid']);
+        $fieldTable->updateData(['columnname' => 'creator_user_id', 'fieldname' => 'creator_user_id'], ['columnname' => 'smcreatorid']);
+        $fieldTable->updateData(['columnname' => 'createdtime', 'fieldname' => 'createdtime'], ['columnname' => 'createdtime']);
+        $fieldTable->updateData(['columnname' => 'modifiedtime', 'fieldname' => 'modifiedtime'], ['columnname' => 'modifiedtime']);
     }
 }
