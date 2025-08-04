52d6bb55114865c5b4be63a7432f6251f434a5c8
Martin Jurcisin <jurcisin@its4you.sk>
2025-08-04 13:20:45 +0200

    Licensing and code format

--- modules/Users/models/Record.php
+++ modules/Users/models/Record.php
@@ -7,6 +7,7 @@
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
  *************************************************************************************/
+
 /**
  * This file is part of Defalto – a CRM software developed by IT-Solutions4You s.r.o.
  *
@@ -15,7 +16,6 @@
  * These contributions are licensed under the GNU AGPL v3 License.
  * See LICENSE-AGPLv3.txt for more details.
  */
-
 class Users_Record_Model extends Vtiger_Record_Model
 {
     public static int $searchUsersLimit = 20;
