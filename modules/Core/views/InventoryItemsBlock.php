b8dd890c95e61122bb161407ddcb6f4e2a9d5a5e
Errorsupport <errors.pport@gmail.com>
2025-08-28 15:00:35 +0200

    #168 - Comments remove

--- modules/Core/views/InventoryItemsBlock.php
+++ modules/Core/views/InventoryItemsBlock.php
@@ -1,13 +1,4 @@
 <?php
-/*
- * This file is part of the IT-Solutions4You CRM Software.
- *
- * (c) IT-Solutions4You s.r.o [info@its4you.sk]
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 /**
  * This file is part of Defalto – a CRM software developed by IT-Solutions4You s.r.o.
  *
