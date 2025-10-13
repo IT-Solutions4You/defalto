8cd6f1054845b625025750218df18ab4da715f28
Errorsupport <errors.pport@gmail.com>
2025-10-13 14:46:03 +0200

    #168 - System configuration validation step

--- modules/Install/models/Utils.php
+++ modules/Install/models/Utils.php
@@ -201,6 +201,37 @@ class Install_Utils_Model
         return $directiveValues;
     }
 
+    public static function getErrorReportingLevelToString(int $level): string {
+        $map = [
+            E_ERROR             => 'E_ERROR',
+            E_WARNING           => 'E_WARNING',
+            E_PARSE             => 'E_PARSE',
+            E_NOTICE            => 'E_NOTICE',
+            E_CORE_ERROR        => 'E_CORE_ERROR',
+            E_CORE_WARNING      => 'E_CORE_WARNING',
+            E_COMPILE_ERROR     => 'E_COMPILE_ERROR',
+            E_COMPILE_WARNING   => 'E_COMPILE_WARNING',
+            E_USER_ERROR        => 'E_USER_ERROR',
+            E_USER_WARNING      => 'E_USER_WARNING',
+            E_USER_NOTICE       => 'E_USER_NOTICE',
+            E_STRICT            => 'E_STRICT',
+            E_RECOVERABLE_ERROR => 'E_RECOVERABLE_ERROR',
+            E_DEPRECATED        => 'E_DEPRECATED',
+            E_USER_DEPRECATED   => 'E_USER_DEPRECATED',
+            E_ALL               => 'E_ALL'
+        ];
+
+        $result = [];
+        foreach ($map as $const => $name) {
+            if (($level & $const) === $const) {
+                $result[] = $name;
+            }
+        }
+
+        return implode(' | ', $result);
+    }
+
+
     /**
      * Variable has the recommended php settings for smooth running of vtigerCRM
      * @var array
