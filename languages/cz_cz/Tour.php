87ee52414f58770d34ee6086f22e576556263548
Errorsupport <errors.pport@gmail.com>
2026-01-08 15:40:35 +0100

    #246 Missing language files for cz_cz and de_de

---
+++ languages/cz_cz/Tour.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * This file is part of Defalto – a CRM software developed by IT-Solutions4You s.r.o.
+ *
+ * Modifications and additions by IT-Solutions4You (ITS4YOU) are Copyright (c) IT-Solutions4You s.r.o.
+ *
+ * These contributions are licensed under the GNU AGPL v3 License.
+ * See LICENSE-AGPLv3.txt for more details.
+ */
+$languageStrings = [
+    'SINGLE_Tour' => 'Prehliadka',
+    'LBL_READY_TO_USE_CRM' => 'Defalto CRM je teraz pripravený na použitie!',
+    'LBL_WHAT_TO_DO_NEXT' => 'Čo by ste chceli urobiť ďalej?',
+    'LBL_VISIT_CRM' => 'Navštívte našu stránku Defalto CRM',
+    'LBL_VISIT_CRM_DESC' => 'Navštívte našu stránku Defalto CRM, kde nájdete užitočné informácie a návody, ktoré vám pomôžu začať používať náš systém.',
+    'LBL_DOCS' => 'Dokumentácia',
+    'LBL_DOCS_DESC' => 'Objavte našu podrobnú dokumentáciu a krok za krokom návody, ktoré vám pomôžu jednoducho používať systém Defalto CRM.',
+    'LBL_FORUMS' => 'Fóra',
+    'LBL_FORUMS_DESC' => 'Pripojte sa ku komunite Defalto CRM, diskutujte a zdieľajte nápady',
+    'LBL_QUICK_START' => 'Rýchly štart',
+    'LBL_QUICK_START_DESC' => 'Krok za krokom inštrukcie na rýchle nastavenie a začatie používania Defalto CRM',
+    'LBL_GUIDES' => 'Návody',
+    'LBL_START_GUIDE' => 'Začať s návodom',
+    'LBL_DELETE_DEMO_DATA' => 'Odstrániť demo dáta',
+    'LBL_IMPORT_DEMO_DATA' => 'Importovať demo dáta',
+    'LBL_FIRST_STEP' => 'Prvý krok',
+    'LBL_IMPORT_DATA_REQUIRED' => 'Import demo dát je potrebný na začatie návodu',
+    'LBL_CONTINUE_WITH_GUIDE' => 'Pokračujte v návode kliknutím na tlačidlo',
+    'LBL_PREV_STEP' => 'Predchádzajúci krok',
+    'LBL_NEXT_STEP' => 'Ďalší krok',
+    'LBL_FINISH_GUIDE' => 'Dokončiť návod',
+    'LBL_DELETE_DATA_AFTER_FINISH' => 'Demo dáta môžete odstrániť po dokončení návodu',
+    'Index' => 'Pripravené na použitie',
+    'Guide' => 'Návod',
+    'Quick Start Guide' => 'Rýchly začiatok s aplikáciou',
+    'This guide will help you quickly get started with the application. It will walk you through the basic features and functionalities.' => 'Táto príručka vám pomôže rýchlo začať s aplikáciou. Prevedie vás základnými funkciami a funkciami.',
+    'Welcome to the Quick Start Guide! Lets begin with the first step.' => 'Vitajte v príručke pre rýchly štart! Začnime prvým krokom.',
+    'You will learn how to navigate through the application and understand its layout.' => 'Naučíte sa orientovať v aplikácii a pochopíte jej rozloženie.',
+    'Menu' => 'Menu',
+    'Left Side Menu' => 'Ponuka na ľavej strane',
+    'There is a menu on the top left side where you can access different modules sorted in categories.' => 'V ľavej hornej časti je ponuka, kde máte prístup k rôznym modulom zoradeným podľa kategórií.',
+    'There is a left side menu where you can access different modules from current category.' => 'K dispozícii je ľavé bočné menu, kde máte prístup k rôznym modulom z aktuálnej kategórie.',
+    'You are now viewing the Contact record edit screen.' => 'Teraz vidíte obrazovku úpravy záznamu kontaktu.',
+    'Here, you can update details and manage information with ease.' => 'Tu môžete jednoducho aktualizovať podrobnosti a spravovať informácie.',
+    'Blocks' => 'Bloky',
+    'You can view various sections, each grouping related fields together for easier editing.' => 'Môžete si prezerať rôzne sekcie, z ktorých každá zoskupuje súvisiace polia pre jednoduchšie úpravy.',
+    'Buttons' => 'Tlačidla',
+    'At the bottom of the window, you will find the Cancel and Save buttons to either discard or save your changes.' => 'V spodnej časti okna nájdete tlačidlá Zrušiť a Uložiť na zrušenie alebo uloženie zmien.',
+    'You are now viewing the Contact record detail screen.' => 'Teraz sa zobrazuje obrazovka podrobností záznamu kontaktu.',
+    'Here, you can view all the details of the Contact record.' => 'Tu si môžete pozrieť všetky podrobnosti záznamu kontaktu.',
+    'You can perform actions like Edit, Delete, and more from the buttons available on the top right side.' => 'Pomocou tlačidiel v pravej hornej časti môžete vykonávať akcie ako Upraviť, Odstrániť a ďalšie.',
+    'Actions' => 'Akcie',
+    'Tabs' => 'Navigácia',
+    'You can switch between different tabs to view related information.' => 'Môžete prepínať medzi rôznymi kartami a zobraziť súvisiace informácie.',
+];
+$jsLanguageStrings = [
+];
---
+++ languages/de_de/Settings/InventoryItem.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of Defalto – a CRM software developed by IT-Solutions4You s.r.o.
+ *
+ * Modifications and additions by IT-Solutions4You (ITS4YOU) are Copyright (c) IT-Solutions4You s.r.o.
+ *
+ * These contributions are licensed under the GNU AGPL v3 License.
+ * See LICENSE-AGPLv3.txt for more details.
+ */
+$languageStrings = [
+    'InventoryItem' => 'Inventory Item',
+    'SINGLE_InventoryItem' => 'Inventory Item',
+    'LBL_GENERAL_SETTING' => 'General Setting',
+    'LBL_FIELDS_IN_ITEMS_BLOCK' => 'Fields in Items Block',
+    'LBL_SELECT_VISIBLE_FIELDS' => 'Select Visible Fields',
+];
+$jsLanguageStrings = [
+];
