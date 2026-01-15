caa1606de533a70567299803ac0f4d942435ce2d
Martin Jurcisin <jurcisin@its4you.sk>
2026-01-15 19:34:11 +0100

    #266 Improved handling of recurring Invoices generated from Sales Order

---
+++ modules/Invoice/models/SORecurring.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * This file is part of Defalto – a CRM software developed by IT-Solutions4You s.r.o.
+ *
+ * Modifications and additions by IT-Solutions4You (ITS4YOU) are Copyright (c) IT-Solutions4You s.r.o.
+ *
+ * These contributions are licensed under the GNU AGPL v3 License.
+ * See LICENSE-AGPLv3.txt for more details.
+ */
+
+class Invoice_SORecurring_Model
+{
+    /**
+     * @param int    $salesOrderId
+     * @param string $recurringDate
+     * @param int    $paymentDuration
+     *
+     * @return void
+     */
+    public static function run(int $salesOrderId, string $recurringDate, int $paymentDuration): void
+    {
+        global $current_user, $log;
+
+        if (!$recurringDate) {
+            $recurringDate = date('Y-m-d');
+        }
+
+        if (!$current_user) {
+            $current_user = Users::getActiveAdminUser();
+        }
+
+        if (!$log) {
+            $log = Logger::getLogger('RecurringInvoice');
+        }
+
+        $soFocus = CRMEntity::getInstance('SalesOrder');
+        $soFocus->id = $salesOrderId;
+        $soFocus->retrieve_entity_info($salesOrderId, 'SalesOrder');
+
+        $focus = CRMEntity::getInstance('Invoice');
+        $focus->column_fields['salesorder_id'] = $salesOrderId;
+
+        foreach ($soFocus->column_fields as $fieldName => $fieldValue) {
+            if (isset($focus->column_fields[$fieldName])) {
+                $focus->column_fields[$fieldName] = decode_html($fieldValue);
+            }
+        }
+
+        $focus->id = '';
+        $focus->mode = '';
+        $focus->column_fields['invoicestatus'] = 'AutoCreated';
+        $focus->column_fields['invoicedate'] = $recurringDate;
+
+        [$y, $m, $d] = explode('-', $recurringDate);
+        $focus->column_fields['duedate'] = date('Y-m-d', mktime(0, 0, 0, $m, (int)$d + $paymentDuration, $y));
+
+        $focus->_salesorderid = $salesOrderId;
+        $focus->column_fields['source'] = 'RECURRING INVOICE';
+
+        try {
+            $focus->save('Invoice');
+        } catch (Exception $e) {
+            $log->info($e->getMessage());
+        }
+    }
+}
