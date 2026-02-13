109568133bc18991a355e12cbfb29d06f58a108d
Errorsupport <errors.pport@gmail.com>
2026-02-13 08:25:26 +0100

    #266 Faq picklist colors

--- modules/Faq/models/Install.php
+++ modules/Faq/models/Install.php
@@ -119,10 +119,10 @@ class Faq_Install_Model extends Core_Install_Model
                     'summaryfield' => 0,
                     'headerfield' => 0,
                     'picklist_values' => [
-                        ['Draft', '', 0],
-                        ['Reviewed', '', 0],
-                        ['Published', '', 0],
-                        ['Obsolete', '', 0],
+                        ['Draft', '#E5E7EB', 0],
+                        ['Reviewed', '#5FA6FF', 0],
+                        ['Published', '#5CE573', 0],
+                        ['Obsolete', '#6B7280', 0],
                     ],
                 ],
                 'product_id' => [
