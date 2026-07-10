1de7ee1a268b840e1232dfcefe3d2f4cb0a56914
Errorsupport <errors.pport@gmail.com>
2026-07-10 12:16:06 +0200

    Core 1.7: Add quick create sequence management for fields and bump version to 1.7.1

--- version.php
+++ version.php
@@ -19,5 +19,5 @@
 
 $patch_version = '2607081003';
 $modified_database = '';
-$defalto_display_version = $defalto_current_version = '1.7.0';
+$defalto_display_version = $defalto_current_version = '1.7.1';
 $_SESSION['defalto_version'] = $defalto_current_version;
