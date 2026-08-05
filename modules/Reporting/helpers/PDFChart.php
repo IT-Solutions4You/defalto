2d27df8bbbdf958ad36759585f7fa4ba8d697b1c
Errorsupport <errors.pport@gmail.com>
2026-08-05 12:10:56 +0200

    Fix UTF-8 encoding issue in license header of `PDFChart.php`

--- modules/Reporting/helpers/PDFChart.php
+++ modules/Reporting/helpers/PDFChart.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This file is part of Defalto â€“ a CRM software developed by IT-Solutions4You s.r.o.
+ * This file is part of Defalto – a CRM software developed by IT-Solutions4You s.r.o.
  *
  * (c) IT-Solutions4You s.r.o
  *
