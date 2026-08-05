5cdc23d8ccae4327d1337753cfb3e673186c73c9
Errorsupport <errors.pport@gmail.com>
2026-08-05 11:55:47 +0200

    Remove duplicate license header from `Table.js` in Reporting module

--- layouts/d1/modules/Reporting/resources/Table.js
+++ layouts/d1/modules/Reporting/resources/Table.js
@@ -1,21 +1,3 @@
-/*
- * This file is part of Defalto – a CRM software developed by IT-Solutions4You s.r.o.
- *
- * (c) IT-Solutions4You s.r.o
- *
- * This file is licensed under the GNU AGPL v3 License.
- * See LICENSE-AGPLv3.txt for more details.
- */
-
-/*
- * This file is part of Defalto – a CRM software developed by IT-Solutions4You s.r.o.
- *
- * (c) IT-Solutions4You s.r.o
- *
- * This file is licensed under the GNU AGPL v3 License.
- * See LICENSE-AGPLv3.txt for more details.
- */
-
 /**
  * This file is part of Defalto – a CRM software developed by IT-Solutions4You s.r.o.
  *
