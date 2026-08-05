3aea14feea21aa451cf04a87d3ae5ab979441223
Errorsupport <errors.pport@gmail.com>
2026-08-05 12:00:55 +0200

    Remove duplicate license header from `Grouping.php` in Reporting module

--- modules/Reporting/uitypes/Grouping.php
+++ modules/Reporting/uitypes/Grouping.php
@@ -8,24 +8,6 @@
  * See LICENSE-AGPLv3.txt for more details.
  */
 
-/*
- * This file is part of Defalto – a CRM software developed by IT-Solutions4You s.r.o.
- *
- * (c) IT-Solutions4You s.r.o
- *
- * This file is licensed under the GNU AGPL v3 License.
- * See LICENSE-AGPLv3.txt for more details.
- */
-
-/**
- * This file is part of Defalto â€“ a CRM software developed by IT-Solutions4You s.r.o.
- *
- * (c) IT-Solutions4You s.r.o
- *
- * This file is licensed under the GNU AGPL v3 License.
- * See LICENSE-AGPLv3.txt for more details.
- */
-
 class Reporting_Grouping_UIType extends Vtiger_Base_UIType
 {
     public function getTemplateName(): string
