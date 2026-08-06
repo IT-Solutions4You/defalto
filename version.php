2c7e746cd90be6ade2876ad0b949585ece5393cc
Errorsupport <errors.pport@gmail.com>
2026-08-06 10:26:57 +0200

    Exclude duplicate relation activities in history tracking and bump version to 1.8.0.

--- version.php
+++ version.php
@@ -17,7 +17,7 @@
  * See LICENSE-AGPLv3.txt for more details.
  */
 
-$patch_version = '2608050005';
+$patch_version = '2608060010';
 $modified_database = '';
 $defalto_display_version = $defalto_current_version = '1.8.0';
 $_SESSION['defalto_version'] = $defalto_current_version;
