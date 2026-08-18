d5996748d56fe75d8f95768b2d78d73981ee5e32
Errorsupport <errors.pport@gmail.com>
2026-08-18 13:23:40 +0200

    Add `addAnyFieldContainsCondition` method in `QueryGenerator` and update version to `1.8.88`

--- version.php
+++ version.php
@@ -17,7 +17,7 @@
  * See LICENSE-AGPLv3.txt for more details.
  */
 
-$patch_version = '2608180025';
+$patch_version = '2608180026';
 $modified_database = '';
-$defalto_display_version = $defalto_current_version = '1.8.87';
+$defalto_display_version = $defalto_current_version = '1.8.88';
 $_SESSION['defalto_version'] = $defalto_current_version;
