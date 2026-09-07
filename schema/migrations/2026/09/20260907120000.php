f0c256c5fe19001f5a58f31b63d358d37e4533f2
Errorsupport <errors.pport@gmail.com>
2026-09-07 14:32:41 +0200

    Refactor `installLink` method for menu items and replace migration logic for `LBL_SUMMARY_WIDGET_EDITOR`. Update version to 1.9.0.

--- schema/migrations/2026/09/20260907120000.php
+++ schema/migrations/2026/09/20260907120000.php
@@ -13,24 +13,7 @@ if (!class_exists('Migration_20260907120000')) {
     {
         public function migrate(string $fileName): void
         {
-            $blockName = 'LBL_MODULE_MANAGER';
-            $label = 'LBL_SUMMARY_WIDGET_EDITOR';
-            $menu = Settings_Vtiger_Menu_Model::createMenu($blockName);
-
-            foreach (Settings_Vtiger_MenuItem_Model::$defaultMenuItemLinks[$blockName] as $sequence => $definition) {
-                if ($definition[0] === $label) {
-                    Settings_Vtiger_MenuItem_Model::createItem(
-                        $label,
-                        $definition[1],
-                        $menu,
-                        $definition[2],
-                        $sequence
-                    );
-                    Settings_Vtiger_MenuItem_Model::activate($label);
-
-                    break;
-                }
-            }
+            Settings_Vtiger_MenuItem_Model::installLink('LBL_SUMMARY_WIDGET_EDITOR');
         }
     }
 } else {
