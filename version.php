c225038460ba9fea8c751fb4909622567f9ab5b7
Errorsupport <errors.pport@gmail.com>
2026-09-15 12:55:44 +0200

    Workflows: add support for creation context in evaluation, refine change condition handling for new records, clone workflows for isolated evaluation, and enhance documentation

--- version.php
+++ version.php
@@ -17,7 +17,7 @@
  * See LICENSE-AGPLv3.txt for more details.
  */
 
-$patch_version = '2608260072';
+$patch_version = '2608260077';
 $modified_database = '';
 $defalto_display_version = $defalto_current_version = '1.9.31';
 $_SESSION['defalto_version'] = $defalto_current_version;
